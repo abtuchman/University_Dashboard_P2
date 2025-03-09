6e9747d61748b5a0a184cda973dd043530656541
abtuchman <abtuchman46@gmail.com>
2025-03-09 19:22:01 -0400

    Update example1_test.py

--- example1_test.py
+++ example1_test.py
@@ -7,9 +7,25 @@ import pandas as pd
 # Title of the app
 st.title("Streamlit Dashboard with Plotly")
 
-url='https://github.com/abtuchman/University_Dashboard_P2/blob/main/university_student_dashboard_data.csv'
+url="https://github.com/abtuchman/University_Dashboard_P2/blob/c9eb52f7d7d49ac300374da7cae0c75124f39d33/university_student_dashboard_data.csv?raw=true"
 uni=pd.read_csv(url)
 
+#convert year and term into one column
+uni['Year'] = pd.to_datetime(uni['Year'], format='%Y')
+uni['Year_Term'] = uni['Year'].dt.strftime('%Y') + ' ' + uni['Term']
+
+
+#Plot admissions raw data
+fig=px.line(uni, x='Year_Term', y=uni.columns[2:5],markers=True,color_discrete_sequence=["red", "blue", "green"])
+fig.update_layout(
+    title='Admissions Over Time',
+    xaxis_title='Year and Term',
+    yaxis_title='Count',
+    yaxis_range=[0, max(uni['Applications'])+500],
+    legend={'title_text':''}
+)
+fig.update_xaxes(tickangle=45)
+fig.show()
 #convert year and term into one column
 uni['Year'] = pd.to_datetime(uni['Year'], format='%Y')
 uni['Year_Term'] = uni['Year'].dt.strftime('%Y') + ' ' + uni['Term']
